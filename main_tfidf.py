910837754c56cef524189efc9030f17413e33712
佐久間大空 <daikisakuma6040@gmail.com>
2021-05-10 23:01:08 +0900

    5-10

--- main_tfidf.py
+++ main_tfidf.py
@@ -18,10 +18,6 @@ minNum = min(lenArr)
 
 allArr = [[0.0] * minNum for i in range(10)]
 
-# allArr = [][]
-
-# allArr = np.array([[0] * minNum for i in range(10)])
-
 i = 0
 j = 0
 
@@ -29,7 +25,6 @@ for document in input_documents:
     print('Document: ', document.rstrip())
     print(docTFIDFdata[document])
     for kw in docTFIDFdata[document].keys():
-        print(docTFIDFdata[document][kw])
         if j < minNum:
             allArr[i][j] = docTFIDFdata[document][kw]
         j += 1
@@ -38,9 +33,12 @@ for document in input_documents:
     i += 1
     j = 0
 
-# X = np.array([0.789, 0.515, 0.335, 0])
-# Y = np.array([0.832, 0.555, 0, 0])
-#
-# print(cos_sim(X, Y))
+res = []
 
-print(allArr)
+for i in range(len(allArr)):
+    for j in range(i+1, 10):
+        x = np.array(allArr[i])
+        y = np.array(allArr[j])
+        res.append(cos_sim(x, y))
+
+print(res)
